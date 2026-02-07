f0c0a1a68be44dddab13dd9cc89bba2c4e803a34
AssemblyAdmin <dpvccm@gmail.com>
2026-02-07 12:06:16 +0000

    Registo com formulários e dados guardados (não duradouros)

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
+
+usuarios = []
 
 pages = ['home', 'login', 'register', 'play']
 
@@ -12,14 +14,37 @@ def home():
     return render_template("index.html", pages=pages)
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     return render_template("login.html", pages=pages)
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template("register.html", pages=pages)
+
+    if request.method == 'POST':
+
+        username = request.form['name']
+        password = request.form['password']
+
+        for u in usuarios:
+
+            if u[0] == username and u[1] == password:
+                msg_add = "Already existing account!"
+                return render_template("register.html", pages=pages, msg=msg_add)
+
+        if username == "" or password == "":
+            msg_add = "Are you serious?"
+            return render_template("register.html", pages=pages, msg=msg_add)
+
+        msg_add = "Account created!"
+        usuarios.append((username, password))
+
+        return render_template("register.html", pages=pages, msg=msg_add)
+
+    if request.method == 'GET':
+
+        return render_template("register.html", pages=pages)
 
 
 @app.route('/play')
